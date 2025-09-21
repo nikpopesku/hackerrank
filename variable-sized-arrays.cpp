a2e05ddc5449e650e567326d69be26cfddd7c71d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-21 16:44:25 +0800

    variable-sized-arrays.cpp

--- variable-sized-arrays.cpp
+++ variable-sized-arrays.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    vector<int> a[10000];
+    vector<int> a[100000];
     int n, q, k;
     cin >> n >> q;
     for (int i = 0; i < n; ++i)
