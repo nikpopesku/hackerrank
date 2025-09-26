e5a0c1133c8620fe055e1cd7e5e68a8e64b70177
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-26 14:45:35 +0800

    virtual-functions.cpp

--- virtual-functions.cpp
+++ virtual-functions.cpp
@@ -2,66 +2,58 @@
 using namespace std;
 
 class Person {
+protected:
     string name;
     int age = 0;
 
+public:
     virtual void putdata() {
-
     }
 
-    virtual string getdata() {}
+    virtual string getdata() {
+    }
 };
 
 
-class Student: Person {
+class Student : Person {
     int marks[1000] = {};
 
     void getdata() override {
+        cin >> name >> age;
     }
 };
 
-class Professor: Person {
-    int publications[1000];
+class Professor : Person {
+    int publications;
 
 public:
-    int publications() {
-        return 4;
-    }
-
     static int cur_id() {
         return 5;
     }
 
-    void getdata() {
-
+    void getdata() override {
+        cin >> name >> age >> publications;
     }
 };
 
 
-
-int main(){
-
+int main() {
     int n, val;
-    cin>>n; //The number of objects that is going to be created.
+    cin >> n; //The number of objects that is going to be created.
     Person *per[n];
 
-    for(int i = 0;i < n;i++){
-
-        cin>>val;
-        if(val == 1){
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        if (val == 1) {
             // If val is 1 current object is of type Professor
             per[i] = new Professor;
-
-        }
-        else per[i] = new Student; // Else the current object is of type Student
+        } else per[i] = new Student; // Else the current object is of type Student
 
         per[i]->getdata(); // Get the data from the user.
-
     }
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
         per[i]->putdata(); // Print the required output for each object.
 
     return 0;
-
 }
