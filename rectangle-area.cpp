acb1c404e886d382ddab1a09e9a74e19896bbfae
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-30 13:58:07 +0800

    rectangle-area.cpp

---
+++ rectangle-area.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+protected:
+    int height = 0, width = 0;
+
+public:
+    void display() const
+    {
+        cout << height << " " << width << "\n";
+    }
+};
+
+class RectangleArea : Rectangle
+{
+public:
+    void read_input()
+    {
+        cin >> height >> width;
+    }
+
+    void display() const
+    {
+        cout << height * width << "\n";
+    }
+};
+
+int main() {
+    RectangleArea r_area;
+
+    r_area.read_input();
+    r_area.display();
+
+    return 0;
+}
