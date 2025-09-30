6ab86c30e1fe27b2cf6a462b473ad2af0a3fb01f
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-30 16:15:11 +0800

    c-class-templates.cpp

--- c-class-templates.cpp
+++ c-class-templates.cpp
@@ -1,12 +1,47 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
+template <class T>
+class AddElements
+{
+    T element;
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+public:
+    T add(T another)
+    {
+        return element + another;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string type;
+        cin >> type;
+        if (type == "float")
+        {
+            double element1, element2;
+            cin >> element1 >> element2;
+            AddElements<double> myfloat(element1);
+            cout << myfloat.add(element2) << endl;
+        }
+        else if (type == "int")
+        {
+            int element1, element2;
+            cin >> element1 >> element2;
+            AddElements<int> myint(element1);
+            cout << myint.add(element2) << endl;
+        }
+        else if (type == "string")
+        {
+            string element1, element2;
+            cin >> element1 >> element2;
+            AddElements<string> mystring(element1);
+            cout << mystring.concatenate(element2) << endl;
+        }
+    }
     return 0;
 }
