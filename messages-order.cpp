e1853f6123445fa99617964c0e74a7bdd8b329c7
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-03 14:03:29 +0800

    messages-order.cpp

--- messages-order.cpp
+++ messages-order.cpp
@@ -4,61 +4,102 @@
 
 using namespace std;
 
-class Message {
-public:
-    Message() {}
-    const string& get_text() {
+class Message
+{
+    string text_;
+    int order_;
+    int static counter_order_;
 
+public:
+    Message() : order_(++counter_order_)
+    {
+    }
+
+    explicit Message(const string& text) : text_(text), order_(++counter_order_)
+    {
+    }
+
+    const string& get_text()
+    {
+        return text_;
+    }
+
+    bool operator<(const Message& other) const
+    {
+        return order_ < other.order_;
     }
 };
 
-class MessageFactory {
+class MessageFactory
+{
 public:
-    MessageFactory() {}
-    Message create_message(const string& text) {
+    MessageFactory()
+    {
+    }
 
+    Message create_message(const string& text)
+    {
+        Message m(text);
+
+        return m;
     }
 };
 
-class Recipient {
+class Recipient
+{
 public:
-    Recipient() {}
-    void receive(const Message& msg) {
+    Recipient()
+    {
+    }
+
+    void receive(const Message& msg)
+    {
         messages_.push_back(msg);
     }
-    void print_messages() {
+
+    void print_messages()
+    {
         fix_order();
-        for (auto& msg : messages_) {
+        for (auto& msg : messages_)
+        {
             cout << msg.get_text() << endl;
         }
         messages_.clear();
     }
+
 private:
-    void fix_order() {
+    void fix_order()
+    {
         sort(messages_.begin(), messages_.end());
     }
+
     vector<Message> messages_;
 };
 
-class Network {
+class Network
+{
 public:
-    static void send_messages(vector<Message> messages, Recipient& recipient) {
+    static void send_messages(vector<Message> messages, Recipient& recipient)
+    {
         // simulates the unpredictable network, where sent messages might arrive in unspecified order
         random_shuffle(messages.begin(), messages.end());
-        for (auto msg : messages) {
+        for (auto msg : messages)
+        {
             recipient.receive(msg);
         }
     }
 };
 
+int Message::counter_order_ = 0;
 
-
-int main() {
+int main()
+{
     MessageFactory message_factory;
     Recipient recipient;
     vector<Message> messages;
     string text;
-    while (getline(cin, text)) {
+    while (getline(cin, text))
+    {
         messages.push_back(message_factory.create_message(text));
     }
     Network::send_messages(messages, recipient);
