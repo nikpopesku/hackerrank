a73dd4b1932a8a27cc81c17e536d0d6049ac9f3b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-03 11:35:22 +0800

    hotel-prices.cpp

---
+++ hotel-prices.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class HotelRoom
+{
+public:
+    HotelRoom(const int bedrooms, const int bathrooms)
+        : bedrooms_(bedrooms), bathrooms_(bathrooms)
+    {
+    }
+
+    [[nodiscard]] int get_price() const
+    {
+        return 50 * bedrooms_ + 100 * bathrooms_;
+    }
+
+private:
+    int bedrooms_;
+    int bathrooms_;
+};
+
+class HotelApartment : public HotelRoom
+{
+public:
+    HotelApartment(const int bedrooms, const int bathrooms): bed
+        : HotelRoom(bedrooms, bathrooms)
+    {
+    }
+
+    [[nodiscard]] int get_price() const
+    {
+        int xxxx = HotelRoom::get_price();
+        return HotelRoom::get_price() + 100;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<HotelRoom*> rooms;
+    for (int i = 0; i < n; ++i)
+    {
+        string room_type;
+        int bedrooms;
+        int bathrooms;
+        cin >> room_type >> bedrooms >> bathrooms;
+        if (room_type == "standard")
+        {
+            rooms.push_back(new HotelRoom(bedrooms, bathrooms));
+        }
+        else
+        {
+            rooms.push_back(new HotelApartment(bedrooms, bathrooms));
+        }
+    }
+
+    int total_profit = 0;
+    for (const auto room : rooms)
+    {
+        total_profit += room->get_price();
+    }
+    cout << total_profit << endl;
+
+    for (const auto room : rooms)
+    {
+        delete room;
+    }
+    rooms.clear();
+
+    return 0;
+}
