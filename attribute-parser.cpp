b25b8a60b2e5de182705f51fbc080a99936dccba
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-07 12:33:31 +0800

    attribute-parser.cpp

--- attribute-parser.cpp
+++ attribute-parser.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int N, Q;
     cin >> N >> Q;
     cin.ignore();
@@ -15,9 +14,7 @@ int main()
     map<string, string> attributes;
     vector<string> tagStack;
 
-    // Parse HRML
-    for (int i = 0; i < N; ++i)
-    {
+    for (int i = 0; i < N; ++i) {
         string line;
         getline(cin, line);
 
@@ -25,21 +22,17 @@ int main()
         string token;
         ss >> token;
 
-        if (token[1] == '/')
-        {
+        if (token[1] == '/') {
             if (!tagStack.empty()) {
                 tagStack.pop_back();
             }
-        }
-        else
-        {
+        } else {
             // Opening tag
             string tagName = token.substr(1); // Remove '<'
 
             // Build current tag path
             string currentPath;
-            for (const auto& tag : tagStack)
-            {
+            for (const auto &tag: tagStack) {
                 currentPath += tag + ".";
             }
             currentPath += tagName;
@@ -47,8 +40,7 @@ int main()
 
             // Parse attributes
             string attrName, equals, attrValue;
-            while (ss >> attrName >> equals >> attrValue)
-            {
+            while (ss >> attrName >> equals >> attrValue) {
                 // Remove quotes from value
                 attrValue = attrValue.substr(1, attrValue.length() - 3);
 
@@ -63,17 +55,13 @@ int main()
     }
 
     // Answer queries
-    for (int i = 0; i < Q; ++i)
-    {
+    for (int i = 0; i < Q; ++i) {
         string query;
         cin >> query;
 
-        if (attributes.contains(query))
-        {
+        if (attributes.contains(query)) {
             cout << attributes[query] << "\n";
-        }
-        else
-        {
+        } else {
             cout << "Not Found!\n";
         }
     }
