3f65ade03bf3fea61b4645ce3b7fab2aef842640
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-07 15:14:32 +0800

    cpp-variadics.cpp

--- cpp-variadics.cpp
+++ cpp-variadics.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 template<bool... digits>
-int reversed_binary_value<digits...>() {
-    return 4;
-};
+int reversed_binary_value() {
+    bool arr[] = {digits...};
+    int result = 0;
+    int power = 1;
+    for (int i = sizeof...(digits) - 1; i >= 0; --i) {
+        if (arr[i]) {
+            result += power;
+        }
+        power *= 2;
+    }
+    return result;
+}
 
 
 template<int n, bool... digits>
@@ -17,7 +26,7 @@ struct CheckValues {
 
 template<bool... digits>
 struct CheckValues<0, digits...> {
-    static void check(int x, int y) {
+    static void check(const int x, const int y) {
         const int z = reversed_binary_value<digits...>();
         std::cout << (z + 64 * y == x);
     }
@@ -34,3 +43,13 @@ int main() {
         cout << "\n";
     }
 }
+
+
+// Sample Input
+// 2
+// 65 1
+// 10 0
+//
+// Sample Output
+// 0100000000000000000000000000000000000000000000000000000000000000
+// 0000000000100000000000000000000000000000000000000000000000000000
