615764e3f3ce519a9a16c24260ae7b31ee7a62f3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-10 13:33:33 +0800

    magic-spells.cpp

--- magic-spells.cpp
+++ magic-spells.cpp
@@ -99,7 +99,7 @@ string SpellJournal::journal;
 
 int longestCommonSubsequence(const string& text1, const string& text2)
 {
-    vector dp(text1.size() + 1, vector<unsigned long long>(text2.size() + 1, 0));
+    vector dp(text1.size() + 1, vector(text2.size() + 1, 0));
     dp[0][0] = 0;
 
     for (auto i = 1; i <= text1.size(); i++)
